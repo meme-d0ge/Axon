3db589d533bc5b73152d86f002801da47f07952c
meme-d0ge <MEME_D0GE@proton.me>
2025-07-05 00:16:23 +0300

    add base MatrixClientProvider

--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -1,15 +1,18 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import '@/app/styles/styles.css';
+import { MatrixClientProvider } from '@/shared/matrix-sdk/MatrixClientProvider.tsx';
 import { ThemeProvider } from '@/shared/Theme/ThemeProvider.tsx';
 
 export const Route = createRootRoute({
 	component: () => (
 		<>
-			<ThemeProvider>
-				<Outlet />
-				<TanStackRouterDevtools />
-			</ThemeProvider>
+			<MatrixClientProvider>
+				<ThemeProvider>
+					<Outlet />
+					<TanStackRouterDevtools />
+				</ThemeProvider>
+			</MatrixClientProvider>
 		</>
 	),
 });
---
+++ src/shared/matrix-sdk/MatrixClientProvider.tsx
@@ -0,0 +1,41 @@
+import type { MatrixClient } from 'matrix-js-sdk/lib/client';
+import type * as React from 'react';
+import { createContext, useContext, useState } from 'react';
+
+interface IMatrixClientProvider {
+	client: MatrixClient | null;
+	setClient: (client: MatrixClient) => void;
+}
+const MatrixClientContext = createContext<IMatrixClientProvider>({
+	client: null,
+	setClient: () => {},
+});
+
+interface IMatrixClientProviderProps {
+	children: React.ReactNode;
+}
+export const MatrixClientProvider = ({
+	children,
+}: IMatrixClientProviderProps) => {
+	const [client, setClient] = useState<MatrixClient | null>(null);
+
+	return (
+		<MatrixClientContext.Provider
+			value={{
+				client,
+				setClient,
+			}}
+		>
+			{children}
+		</MatrixClientContext.Provider>
+	);
+};
+
+export const useClient = () => {
+	const context = useContext(MatrixClientContext);
+
+	if (context === undefined)
+		throw new Error('useClient must be used within a MatrixClientProvider');
+
+	return context;
+};
