2a8dba864a7ec7f49b53ea19d644cf0241f937b8
meme-d0ge <MEME_D0GE@proton.me>
2025-07-04 17:33:44 +0300

    Add Biome linter and tslog logger

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Logger } from 'tslog';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootLogger = new Logger({ name: 'rootLogger' });
+const root = document.getElementById('root');
+
+if (root) {
+	createRoot(root).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+	rootLogger.info(
+		"Application successfully rendered to DOM element with ID 'root'",
+	);
+} else {
+	rootLogger.error(
+		"Failed to render application: DOM element with ID 'root' not found",
+	);
+}
